774681585676701fe787ebf78b2b40e755dd9507
Douglas Denney <denney@lilly.com>
2020-12-15 15:51:29 -0500

    Finished Part 1 day12

--- 2020/Day12/day12.pl
+++ 2020/Day12/day12.pl
@@ -2,42 +2,28 @@
 
 use warnings;
 use strict;
-use feature qw(switch);
 
-sub north {
-  return "N";
-}
-
-sub south {
-  return "S";
-}
-
-sub east {
-  return "E";
-}
-
-sub west {
-  return "W";
-}
-
-sub direction {
-  my $d = @_;
-
-
-}
 my @data = ();
+my $xtmp = 0;
+my $ytmp = 0;
 my $x = 0;
+my $waypointx = 10;
+my $shipx = 0;
 my $y = 0;
+my $waypointy = 1;
+my $shipy = 0;
 my $facing = "E";
-my $degree = "90";
+my $magnitude;
 my $action;
+my $degree = 0;
+my $spin = 0;
 my $value;
 my $item;
+my @compass = ("N", "E", "S", "W");
 
 open(DATA, "data.txt") or die "Could not open data.txt: $!\n";
 @data = <DATA>;
 close (DATA);
-
 chomp @data;
 
 foreach $item (@data) {
@@ -45,19 +31,50 @@ foreach $item (@data) {
   $value = substr ($item, 1);
 
   if (($action eq "L") or ($action eq "R")) {
-    $facing = direction($action, $facing);
-  }
-  elsif ($action eq "F") {
+    SWITCH: {
+      if ($facing eq "N") { $degree = 0; last SWITCH; }
+      if ($facing eq "E") { $degree = 90; last SWITCH; }
+      if ($facing eq "S") { $degree = 180; last SWITCH; }
+      if ($facing eq "W") { $degree = 270; last SWITCH; }
+    }
+    $spin = $degree + $value;
+    if ($spin >= 360) {$spin -= 360};
+
+    SWITCH: {
+      if ($facing eq "N") {last SWITCH;};
+      if ($facing eq "E") {last SWITCH;};
+      if ($facing eq "S") {last SWITCH;};
+      if ($facing eq "W") {last SWITCH;};
+    }
+    if ($spin == 0) {};
+    if ($spin == 90) {};
+    if ($spin == 180) {};
+    if ($spin == 270) {};
+
+    if ($action eq "R") {
+      $facing = $compass[(($degree + $value) / 90) % 4];
+    }
+    elsif ($action eq "L") {
+      $facing = $compass[(($degree + (360 - $value)) / 90) % 4];
+    }
+
 
   }
   else {
-
+    if ($action eq "F") {
+      $action = $facing;
+      $shipx = $shipx + ($waypointx * $value);
+      $shipy = $shipy + ($waypointy * $value);
+    }
   }
-  given($action) {
-    when("N") { $y += $value; print "\$y = $y\n";}
-    when("S") { $y -= $value; print "\$y = $y\n";}
-    when("E") { $x += $value; print "\$x = $x\n";}
-    when("W") { $x -= $value; print "\$x = $x\n";}
-    when("F") { print "Forward $value\n"; }
+
+  SWITCH: {
+    if ($action eq "N") { $y += $value; $waypointy += $value; last SWITCH; }
+    if ($action eq "S") { $y -= $value; $waypointy -= $value; last SWITCH; }
+    if ($action eq "E") { $x += $value; $waypointx += $value; last SWITCH; }
+    if ($action eq "W") { $x -= $value; $waypointx -= $value; last SWITCH; }
   }
 }
+
+print "Part 1: ", abs($x) + abs($y), "\n";
+print "Part 2: ", abs($shipx) + abs($shipy), "\n";
