f525dc0c115c5d767d5dd6ee67ab63c4c2786112
Douglas Denney <denney.douglas@gmail.com>
2021-12-07 21:59:35 -0500

    day03 updates

---
+++ 2021/day04/day04/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  day04
+//
+//  Created by Douglas Denney on 12/7/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
